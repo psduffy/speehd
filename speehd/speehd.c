35a740032fee8fde9e549a7afb587b743386366d
psduffy <psduffy@icloud.com>
2024-01-31 19:40:50 -0600

    Added compatibility for Unix-like & Win32

--- speehd/speehd.c
+++ speehd/speehd.c
@@ -2,23 +2,41 @@
 #include <stdlib.h>
 #include <time.h>
 
+#ifdef _WIN32
+#include <tchar.h>
+#endif
+
 int main() {
     char response;
     FILE* file;
     const int bufferSize = 104857600; // 100 MB
     char* buffer = malloc(bufferSize);
+    if (buffer == NULL) {
+        perror("Failed to allocate memory");
+        return 1;
+    }
     clock_t start, end;
     double cpu_time_used;
-    errno_t err;
 
     printf("Do you want to proceed? (y/n): ");
+
+#ifdef _WIN32
     scanf_s(" %c", &response, 1);
+#else
+    scanf(" %c", &response);
+#endif
 
     if (response == 'y' || response == 'Y') {
         // Write to file
-        err = fopen_s(&file, "testfile", "wb");
+#ifdef _WIN32
+        errno_t err = fopen_s(&file, "testfile", "wb");
         if (err != 0 || file == NULL) {
+#else
+        file = fopen("testfile", "wb");
+        if (file == NULL) {
+#endif
             perror("Error opening file");
+            free(buffer);
             return 1;
         }
 
@@ -33,9 +51,15 @@ int main() {
         printf("Write speed: %f MB/s\n", bufferSize / (cpu_time_used * 1048576));
 
         // Read from file
+#ifdef _WIN32
         err = fopen_s(&file, "testfile", "rb");
         if (err != 0 || file == NULL) {
+#else
+        file = fopen("testfile", "rb");
+        if (file == NULL) {
+#endif
             perror("Error opening file");
+            free(buffer);
             return 1;
         }
 
@@ -48,7 +72,6 @@ int main() {
         cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
         printf("Read time: %f seconds\n", cpu_time_used);
         printf("Read speed: %f MB/s\n", bufferSize / (cpu_time_used * 1048576));
-
     }
     else {
         printf("Operation cancelled.\n");
@@ -57,3 +80,4 @@ int main() {
     free(buffer);
     return 0;
 }
+
